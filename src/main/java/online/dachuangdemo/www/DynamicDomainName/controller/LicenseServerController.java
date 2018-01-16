f96c598fa8dd095ef832bef0eceef7212ec6ce3f
lipengcheng3 <lipengcheng3@jd.com>
2018-01-16 14:45:47 +0800

    add: 添加swagger注释

--- src/main/java/online/dachuangdemo/www/DynamicDomainName/controller/LicenseServerController.java
+++ src/main/java/online/dachuangdemo/www/DynamicDomainName/controller/LicenseServerController.java
@@ -1,5 +1,7 @@
 package online.dachuangdemo.www.DynamicDomainName.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import online.dachuangdemo.www.DynamicDomainName.configuration.ResponseWrapper;
 import online.dachuangdemo.www.DynamicDomainName.domain.LicenseServerVO;
 import org.springframework.stereotype.Controller;
@@ -18,21 +20,25 @@ import java.util.List;
  * description:
  **/
 @Controller
+@Api(value = "/api/licenseServer", description = "注册服务器管理", tags = "License Server")
 @RequestMapping("/api/licenseServer")
 @ResponseBody
 public class LicenseServerController {
-    @RequestMapping(value = "/getAll",method = RequestMethod.GET)
-    public ResponseWrapper<List<LicenseServerVO>> getAllLicenseServer(){
+    @ApiOperation("获得可用的注册服务器信息列表")
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public ResponseWrapper<List<LicenseServerVO>> getAllLicenseServer() {
         return new ResponseWrapper();
     }
 
-    @RequestMapping(value = "/{ServerId}/activation",method = RequestMethod.POST)
-    public ResponseWrapper<Boolean> activeLicenseServer(@PathVariable Integer serverId){
+    @ApiOperation("激活指定的注册服务器")
+    @RequestMapping(value = "/{ServerId}/activation", method = RequestMethod.POST)
+    public ResponseWrapper<Boolean> activeLicenseServer(@PathVariable Integer serverId) {
         return new ResponseWrapper();
     }
 
-    @RequestMapping(value = "/{ServerId}/inactivation",method = RequestMethod.POST)
-    public ResponseWrapper<Boolean> inactiveLicenseServer(@PathVariable Integer serverId){
+    @ApiOperation("关闭指定的注册服务器")
+    @RequestMapping(value = "/{ServerId}/inactivation", method = RequestMethod.POST)
+    public ResponseWrapper<Boolean> inactiveLicenseServer(@PathVariable Integer serverId) {
         return new ResponseWrapper();
     }
 }
